862ed62fdb8fd02852425a4a703a8a2f34fa512a
Bruce <1033684650@qq.com>
2023-05-03 12:29:17 +0800

    finish the reset api between frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app = FastAPI()
 
 # CORS-origins
 origins = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
 # CORS-Middleware
--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -7,8 +7,34 @@ interface IProps {
 }
 
 function Title({ setMessages }: IProps) {
+
+    const [ isResetting, setIsResetting ] = useState(false);
+
+    // Reset the Conversation
+    const resetConversation = async () => {
+        setIsResetting(true);
+
+        await axios.get("http://localhost:8000/reset-messages")
+                    .then((res)=>{
+                        if(res.status === 200) {
+                            setMessages([])
+                        }else{
+                            console.error("There was an error with the API request to backend.")
+                        }
+                    })
+                    .catch((err)=>{
+                        console.error(err.messages)
+                    })
+
+
+        setIsResetting(false);
+
+    }
+
     return (
-        <div>Title</div>
+        <div>
+            <button onClick={resetConversation} className="bg-gray-500 p-5">Reset</button>
+        </div>
     )
 }
 
