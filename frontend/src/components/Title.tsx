621e399d404a0e324ca77bc3d37647cfd76b11e6
Bruce <1033684650@qq.com>
2023-05-03 12:43:04 +0800

    finish the title style and function

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import Resfresh from "../icons/Refesh";
 
 
 interface IProps {
@@ -17,6 +18,7 @@ function Title({ setMessages }: IProps) {
         await axios.get("http://localhost:8000/reset-messages")
                     .then((res)=>{
                         if(res.status === 200) {
+                            alert("Success")
                             setMessages([])
                         }else{
                             console.error("There was an error with the API request to backend.")
@@ -32,8 +34,15 @@ function Title({ setMessages }: IProps) {
     }
 
     return (
-        <div>
-            <button onClick={resetConversation} className="bg-gray-500 p-5">Reset</button>
+        <div className="flex justify-between items-center w-full p-4 bg-pink-600 text-white">
+            <div className="italic">Bruce Chat</div>
+            <button 
+                onClick={resetConversation} 
+                className={"transition-all duration-300 text-white-300 hover:text-yellow-300 " + 
+                (isResetting && "animate-pulse")}
+            >
+                { Resfresh }
+            </button>
         </div>
     )
 }
